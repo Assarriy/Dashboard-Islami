44ab9e7fb5161be2cf94d11d410fef1f2e9988d3
assarriy <wibowoassariy23@gmail.com>
2023-10-16 11:06:24 +0700

    DI-Fitur Zakat

--- app/src/main/java/com/assarriy/dashboardislami/zakat/ZakatActivity.kt
+++ app/src/main/java/com/assarriy/dashboardislami/zakat/ZakatActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.assarriy.dashboardislami.R
 import com.assarriy.dashboardislami.databinding.ActivityZakatBinding
+import java.text.NumberFormat
+import java.util.Locale
 
 class ZakatActivity : AppCompatActivity() {
 
@@ -12,6 +14,10 @@ class ZakatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityZakatBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val actionBar = supportActionBar
+        actionBar!!.title = "Zakat"
+        actionBar.setDisplayHomeAsUpEnabled(true)
 
         initHitungZakat()
     }
@@ -22,21 +28,24 @@ class ZakatActivity : AppCompatActivity() {
             var isEmptyField = false
             val totalHarta = binding.edtTotalKeseluruhanHarta.text.trim().toString()
 
-            if (totalHarta.isEmpty()){
+            if (totalHarta.isEmpty()) {
                 !isEmptyField
                 binding.edtTotalKeseluruhanHarta.error = "Field ini tidak boleh kosong"
             }
 
-            if (!isEmptyField){
+            if (!isEmptyField) {
                 val emas = 92395000
                 val persen = 0.025
+                val localeID = Locale("in", "ID")
+                val formatRupiah: NumberFormat = NumberFormat.getCurrencyInstance(localeID)
 
-                if(totalHarta < emas.toString()) {
+                if (totalHarta < emas.toString()) {
                     binding.tvStatus.text = "Anda belum wajib zakat"
+                    binding.tvZakatYangDikeluarkan.text = "0"
                 } else {
-                    val zakat = totalHarta.toDouble() * persen
+                    val zakat = totalHarta.toInt() * persen
                     binding.tvStatus.text = "Anda wajib membayar zakat"
-                    binding.tvZakatYangDikeluarkan.text = "Rp. $zakat"
+                    binding.tvZakatYangDikeluarkan.text = formatRupiah.format(zakat)
                 }
             }
 
