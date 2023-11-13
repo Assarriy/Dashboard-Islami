43a18739b878f456381937ab61e1aaf6eae5883f
risnanto73 <tiolast63@gmail.com>
2023-11-13 08:29:19 +0700

    add feature kajian youtube

---
+++ app/src/main/java/com/assarriy/dashboardislami/kajian/DetailKajianActivity.kt
@@ -0,0 +1,60 @@
+package com.assarriy.dashboardislami.kajian
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.assarriy.dashboardislami.R
+import com.assarriy.dashboardislami.databinding.ActivityDetailKajianBinding
+import com.assarriy.dashboardislami.kajian.model.KajianModel
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+
+class DetailKajianActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailKajianBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailKajianBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val kajian = intent.getParcelableExtra<KajianModel>(EXTRA_KAJIAN) as KajianModel
+        initKajian(kajian)
+    }
+
+    private fun initKajian(kajian: KajianModel) {
+        binding.tvPenceramah.text = kajian.speaker
+        binding.tvChannelYoutube.text = kajian.channel
+        binding.tvJudulKajian.text = kajian.title
+        binding.tvDeskripsi.text = kajian.summary
+
+        val youtubeURL: YouTubePlayerView = binding.youtubePlayerView
+        lifecycle.addObserver(youtubeURL)
+        youtubeURL.addYouTubePlayerListener(
+            object : AbstractYouTubePlayerListener() {
+                override fun onReady(youTubePlayer: YouTubePlayer) {
+                    youTubePlayer.loadVideo(kajian.urlVideo, 0f)
+                    super.onReady(youTubePlayer)
+                }
+            }
+        )
+
+        binding.fabShare.setOnClickListener {
+            val sendKajian = Intent()
+            sendKajian.action = Intent.ACTION_SEND
+            sendKajian.putExtra(
+                Intent.EXTRA_TEXT, "Kajian dengan judul ${kajian.title}" +
+                        " disampaikan oleh ${kajian.speaker} dapat dilihat di link berikut ini: " +
+                        "https://www.youtube.com/watch?v=${kajian.urlVideo}"
+            )
+            sendKajian.type = "text/plain"
+            startActivity(sendKajian)
+        }
+    }
+
+    companion object {
+        val EXTRA_KAJIAN: String = "extra_kajian"
+    }
+}
--- app/src/main/java/com/assarriy/dashboardislami/kajian/KajianActivity.kt
+++ app/src/main/java/com/assarriy/dashboardislami/kajian/KajianActivity.kt
@@ -4,8 +4,12 @@ package com.assarriy.dashboardislami.kajian
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.assarriy.dashboardislami.R
 import com.assarriy.dashboardislami.databinding.ActivityKajianBinding
+import com.assarriy.dashboardislami.kajian.adapter.KajianAdapter
+import com.assarriy.dashboardislami.kajian.data.KajianData
+import com.assarriy.dashboardislami.kajian.model.KajianModel
 
 class KajianActivity : AppCompatActivity() {
 
@@ -16,6 +20,15 @@ class KajianActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        binding.rvKajian.layoutManager = LinearLayoutManager(this)
+        binding.rvKajian.setHasFixedSize(true)
+        val listKajian = ArrayList<KajianModel>()
+        listKajian.addAll(KajianData.listData)
+        binding.rvKajian.adapter = KajianAdapter(listKajian)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
